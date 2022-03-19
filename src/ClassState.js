a3a3cbc6a13021901b232957dfb9b572e9b194d3
Daniel Córdova <jdany0809@hotmail.com>
2022-03-18 22:32:08 -0500

    Simples states - React.component vs useState

---
+++ src/ClassState.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class ClassState extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: false,
+		};
+	}
+	render() {
+		return (
+			<div>
+				<h2>Eliminar {this.props.name}</h2>
+				<p>
+					Por favor, escribe el codigo de seguridad para comprobar que quieres
+					eleminar esta seccion
+				</p>
+				{this.state.error && <p>Error: El codigo es incorrecto</p>}
+
+				<input placeholder="Codigo de seguridad" />
+				<button
+					onClick={() => {
+						this.setState({ error: !this.state.error });
+					}}
+				>
+					Comprobar
+				</button>
+			</div>
+		);
+	}
+}
+
+export { ClassState };
---
+++ src/UseState.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+function UseState({ name }) {
+	const [error, setError] = useState(false);
+	return (
+		<div>
+			<h2>Eliminar {name}</h2>
+			<p>
+				Por favor, escribe el codigo de seguridad para comprobar que quieres eleminar
+				esta seccion
+			</p>
+			{error && <p>Error: El codigo es incorrecto</p>}
+			<input placeholder="Codigo de seguridad" />
+			<button
+				onClick={() => {
+					setError((prevState) => !prevState);
+				}}
+			>
+				Comprobar
+			</button>
+		</div>
+	);
+}
+
+export { UseState };
