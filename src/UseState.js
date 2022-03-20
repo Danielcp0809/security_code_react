6070ef10e6404f3e188be2891f2c7dc23308c97c
Daniel Córdova <jdany0809@hotmail.com>
2022-03-19 22:34:57 -0500

    semideclarative states using useState

--- src/UseState.js
+++ src/UseState.js
@@ -4,9 +4,6 @@ import React, { useState, useEffect } from "react";
 const SECURITY_CODE = "paradigma";
 
 function UseState({ name }) {
-	// const [error, setError] = useState(false);
-	// const [loading, setLoading] = useState(false);
-	// const [value, setValue] = useState("");
 	// estado compuesto usando useState
 	const [state, setState] = useState({
 		value: "",
@@ -16,27 +13,67 @@ function UseState({ name }) {
 		confirmed: false, // para pasar a la pantalla de confirmacion
 	});
 
+	const onConfirm = () => {
+		/// cuando queramos pasar al estado de confirmacion
+		setState({
+			...state,
+			error: false,
+			loading: false,
+			confirmed: true,
+		});
+	};
+
+	const onError = () => {
+		/// cuando queramos pasar al estado de errpr
+		setState({
+			...state,
+			error: true,
+			loading: false,
+		});
+	};
+
+	const onWrite = (event) => {
+		/// estado de cambio en el input
+		setState({
+			...state,
+			value: event.target.value,
+		});
+	};
+
+	const onCheck = () => {
+		/// estado de comprobacion del codigo
+		setState({
+			...state,
+			loading: true,
+		});
+	};
+
+	const onDelete = () => {
+		/// estado de confirmar la eliminacion del componente
+		setState({
+			...state,
+			deleted: true,
+		});
+	};
+
+	const onReset = () => {
+		/// volver al estado original
+		setState({
+			...state,
+			confirmed: false,
+			deleted: false,
+			value: "",
+		});
+	};
+
 	useEffect(() => {
 		/// useEffect para simular una respuesta del backen
 		if (!!state.loading) {
 			setTimeout(() => {
 				if (state.value !== SECURITY_CODE) {
-					setState({
-						...state,
-						error: true,
-						loading: false,
-					});
-					// setError(true)
-					// setLoading(false)
+					onError();
 				} else {
-					setState({
-						...state,
-						error: false,
-						loading: false,
-						confirmed: true,
-					});
-					// setLoading(false)
-					// setError(false)
+					onConfirm();
 				}
 			}, 2000); /// si loading es true, 3sg despues poner en falso.
 		}
@@ -59,25 +96,9 @@ function UseState({ name }) {
 				<input
 					placeholder="Codigo de seguridad"
 					value={state.value}
-					onChange={(event) => {
-						setState({
-							...state,
-							value: event.target.value,
-						});
-						// setValue(event.target.value)
-					}}
+					onChange={onWrite}
 				/>
-				<button
-					onClick={() => {
-						setState({
-							...state,
-							loading: true,
-						});
-						// setLoading(true)
-					}}
-				>
-					Comprobar
-				</button>
+				<button onClick={onCheck}>Comprobar</button>
 			</div>
 		);
 	} else if (!state.deleted && state.confirmed) {
@@ -85,45 +106,15 @@ function UseState({ name }) {
 			<React.Fragment>
 				<h2>Eliminar {name}</h2>
 				<p>Estas seguro que quieres eliminar {name}?</p>
-				<button
-					onClick={() => {
-						setState({
-							...state,
-							deleted: true,
-						});
-					}}
-				>
-					Si, eliminar
-				</button>
-				<button
-					onClick={() => {
-						setState({
-							...state,
-							confirmed: false,
-							value: "",
-						});
-					}}
-				>
-					No, volver
-				</button>
+				<button onClick={onDelete}>Si, eliminar</button>
+				<button onClick={onReset}>No, volver</button>
 			</React.Fragment>
 		);
 	} else {
 		return (
 			<React.Fragment>
 				<h2>{name} fue eliminado</h2>
-				<button
-					onClick={() => {
-						setState({
-							...state,
-							confirmed: false,
-							deleted: false,
-							value: "",
-						});
-					}}
-				>
-					Recuperar {name}
-				</button>
+				<button onClick={onReset}>Recuperar {name}</button>
 			</React.Fragment>
 		);
 	}
