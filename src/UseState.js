4ee57131fba7f3cae8061a0056e7a71b764de8cc
Daniel Córdova <jdany0809@hotmail.com>
2022-03-19 20:23:13 -0500

    Material UI library added & effects with useEffects

--- src/UseState.js
+++ src/UseState.js
@@ -1,7 +1,19 @@
-import React, { useState } from "react";
+import { CircularProgress } from "@mui/material";
+import React, { useState, useEffect } from "react";
 
 function UseState({ name }) {
 	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		/// useEffect para simular una respuesta del backen
+		if (loading) {
+			setTimeout(() => {
+				setLoading(false);
+			}, 3000); /// si loading es true, 3sg despues poner en falso.
+		}
+	}, [loading]);
+
 	return (
 		<div>
 			<h2>Eliminar {name}</h2>
@@ -9,11 +21,17 @@ function UseState({ name }) {
 				Por favor, escribe el codigo de seguridad para comprobar que quieres eleminar
 				esta seccion
 			</p>
+			{loading && (
+				<div>
+					<CircularProgress />
+					<p>Loading...</p>
+				</div>
+			)}
 			{error && <p>Error: El codigo es incorrecto</p>}
 			<input placeholder="Codigo de seguridad" />
 			<button
 				onClick={() => {
-					setError((prevState) => !prevState);
+					setLoading(true);
 				}}
 			>
 				Comprobar
